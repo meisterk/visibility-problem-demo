953f142ed14de043db98067838bfad0f8abf4ab0
meisterk <franz@kohnlehome.de>
2019-10-16 12:29:00 +0200

    Add Counter class

---
+++ src/Counter.java
@@ -0,0 +1,15 @@
+public class Counter {
+    private int number;
+
+    public Counter() {
+        this.number = 0;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void increment(){
+        number++;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Counter counter = new Counter();
+        counter.increment();
+        System.out.println(counter.getNumber());
     }
 }
