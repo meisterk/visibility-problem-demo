bbc166d21b5e8bb20cf3e575e9bf8f4449daf2ad
meisterk <franz@kohnlehome.de>
2019-10-23 11:53:17 +0200

    Delete Counter

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
         System.out.println("main-thread: second thread is started");
         // let main-Thread sleep
         try {
-            Thread.sleep(2000);
+            Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
--- src/Task.java
+++ src/Task.java
@@ -1,16 +1,21 @@
 public class Task implements Runnable{
-    private boolean loop;
+    private boolean loop; // main-Thread writes to loop, task reads loop
+    //private volatile boolean loop; // volatile: loop is always stored in RAM, not in cache
+    private int count;
 
     public Task() {
         this.loop = true;
+        count = 0;
     }
 
     @Override
     public void run() {
         while (this.loop){
-            System.out.println("second thread is running");
+            //System.out.println("second thread is running");
+            // Don't do any printing here to demonstrate visibility problem
+            count++;
         }
-        System.out.println("end of second thread");
+        System.out.println("End of second thread. Count: " + count);
     }
 
     public void stopTask(){
